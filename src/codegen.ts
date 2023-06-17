f9246a7e9f35eafdd59a68575b2e24f23ecc7045
Alexander Apaza <alrus2797@gmail.com>
2023-06-16 19:52:47 -0500

    feat: add env file

--- src/codegen.ts
+++ src/codegen.ts
@@ -1,7 +1,8 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import { GRAPHQL_URL } from "./constants";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000/functions/index",
+  schema: `${GRAPHQL_URL ?? "http://localhost:4000"}/functions/index`,
   documents: ["src/**/*.tsx"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
